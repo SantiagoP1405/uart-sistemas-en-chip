b2c1c721619672881350d271a19d3b9224ce869b
SantiagoP1405 <116893930+SantiagoP1405@users.noreply.github.com>
2025-05-20 15:47:38 -0600

    MENSAJES DE ARDUINO UNO
    - Programa que imprime en la terminal 7 mensajes distintos cada 5 segundos
    - Configurada la comunicación serial a 9600 baudios

---
+++ mensajes_arduino/mensajes_arduino.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("for i in range");
+  delay(5000);
+  Serial.println("Cortado con nieve, GOD");
+  delay(5000);
+  Serial.println("pan con chilakiles");
+  delay(5000); 
+  Serial.println("patecs");
+  delay(5000);
+  Serial.println("ya vieron a los gansitos?");
+  delay(5000);
+  Serial.println("game day");
+  delay(5000);
+  Serial.println("cuanto falta para semana 5?");
+  delay(5000);
+
+}
